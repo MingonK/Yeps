7bd08d2a9b57e9ccc2550c71673a8d78c2ffd895
Master <Master@Master-PC>
2017-11-27 20:44:49 +0900

    멤버관련 파일 다시 올렸습니다

---
+++ GitYeps/src/main/java/com/yeps/service/MemberMapper.java
@@ -0,0 +1,95 @@
+package com.yeps.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yeps.model.MemberDTO;
+
+@Service
+public class MemberMapper {
+
+	@Autowired
+	private MemberMapper memberMapper;
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public List<MemberDTO> listMember() {
+		return sqlSession.selectList("listMember");
+	}
+	
+	public List<MemberDTO> findMember(String sql){
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("sql", sql);
+		return sqlSession.selectList("findMember",map);
+	}
+
+	public int insertMember(MemberDTO dto) {
+		int res = sqlSession.insert("insertMember",dto);
+		return res;
+	}
+
+	public MemberDTO confirmEmail(String email) {
+		HashMap map = new HashMap();
+		map.put("email", email);
+		try {
+			return sqlSession.selectOne("confirmEmail", map);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public int deleteMember(int mnum) {
+		return sqlSession.delete("deleteMember",mnum);
+	}
+	
+	public MemberDTO loginMember(MemberDTO dto) {
+		try {
+			return sqlSession.selectOne("loginMember", dto);
+		}catch(Exception e) {
+			return null;
+		}
+	}
+	
+	/*
+
+	 public static MemberDTO checkMember(HashMap map) {
+		 SqlSession session = sqlMapper.openSession();
+		 try {
+			 MemberDTO dto = session.selectOne("checkMember", map);
+			 return dto;
+		 }finally {
+			 session.close();
+		 }
+	 }
+
+	 public static MemberDTO getMember(int no) {
+		 SqlSession session = sqlMapper.openSession();
+		 try {
+			 MemberDTO dto = session.selectOne("getMember",no);
+			 return dto;
+		 }finally {
+			 session.close();
+		 }
+	 }
+
+	 public static int updateMember(MemberDTO dto) {
+		 SqlSession session = sqlMapper.openSession();
+		 try {
+			 int res = session.update("updateMember", dto);
+			 session.commit();
+			 return res;
+		 }finally {
+			 session.close();
+		 }
+	 }
+
+	 */
+}
---
+++ GitYeps/src/main/webapp/WEB-INF/views/member/index.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<html>
+<head>
+	<title>YEPS회원관리</title>
+</head>
+<body>
+	<div align="center">
+		<hr color="green" width="300">
+		<h2>YEPS 회원관리 프로그램</h2>
+		<hr color="green" width="300">
+		<table border="1" width="600" height="400">
+			<tr height="50">
+				<th><a href="member_join">회원가입</a></th>
+				<th><a href="member_manager">회원관리</a></th>
+				<th><a href="member_login">로그인</a></th>
+			</tr>
+			<tr>
+				<td colspan="4">아무 사진이나 넣어주세요!!</td>
+			</tr>
+			<tr height="50">
+				<td colspan="4" align="center">
+					YEPS Member
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
