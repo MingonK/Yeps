0d542a491e287e9a0cc157878f1d4202de0153a9
ITBANK403-07 <ITBANK403-07@ITBANK403-07-PC>
2017-12-04 19:42:27 +0900

    레스토랑

--- GitYeps/src/main/java/com/yeps/controller/RestaurantController.java
+++ GitYeps/src/main/java/com/yeps/controller/RestaurantController.java
@@ -1,44 +1,63 @@
 package com.yeps.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.yeps.model.RestaurantDTO;
-import com.yeps.service.RestaurantMapper;
+import com.web.model.RestaurantDTO;
+import com.web.model.uploadFileVO;
+import com.web.service.RestaurantMapper;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class RestaurantController {
+	@Resource(name = "uploadPath")
+	private String uploadPath;
+	
 	
 	@Autowired
 	private RestaurantMapper restaurantMapper;
 	
-	private static final Logger logger = LoggerFactory.getLogger(RestaurantController.class);
-	
-	@RequestMapping(value="/restaurant_sample")
-	public String sampleRest() throws Exception{
-		return "restaurant/Sample";
-	}
-	@RequestMapping(value="/restaurant_jusoPopup")
+
+
+//	private static final Logger logger = LoggerFactory.getLogger(RestaurantController.class);
+
+
+	@RequestMapping(value="/jusoPopup")
 	public String jusoRest() throws Exception{
-		return "jusoPopup";
+		return "restaurant/jusoPopup";
 	}
-	@RequestMapping(value="/result")
-	public String result() throws Exception{
+	@RequestMapping(value="/view")
+	public String view() throws Exception{
 		return "restaurant/result";
 	}
-	
+
 	@RequestMapping(value="/restaurant_insert", method=RequestMethod.GET)
 	public String insertRest() throws Exception {
 		return "restaurant/restaurant_insert";
@@ -46,7 +65,7 @@ public class RestaurantController {
 	@RequestMapping(value="/restaurant_insert", method=RequestMethod.POST)
 	public ModelAndView insertRest(HttpServletRequest req,@ModelAttribute RestaurantDTO dto,BindingResult result) throws Exception {
 		String msg=null,url=null;
-		
+
 		int res=restaurantMapper.insertRest(dto);
 		if(res>0) {
 			msg="레스토랑 등록 성공";
@@ -59,5 +78,100 @@ public class RestaurantController {
 		req.setAttribute("url", url);
 		return new ModelAndView("restaurant/message");
 	}
+
+	@RequestMapping(value="/restaurant_uploadForm2")
+	public String uploadForm() {
+		return "restaurant/restaurant_uploadForm2";
+	}
+
+	@RequestMapping(value="/restaurant_upload")
+	public ModelAndView uploadRest(HttpServletRequest req,@ModelAttribute RestaurantDTO dto, MultipartHttpServletRequest mhsq) throws IllegalStateException,IOException{
+		String msg=null,url=null;
+		String rnum=req.getParameter("rnum");
+		String realFolder = "D:/spring02/upload/";
+		//	         HttpSession session = req.getSession();
+		//	         String upPath = session.getServletContext().getRealPath("/resources/files/");
+
+		File dir = new File(realFolder);
+		if (!dir.isDirectory()) {
+			dir.mkdirs();
+		}
+
+		Map<String, MultipartFile> fileMap = mhsq.getFileMap();
+
+		for (MultipartFile multipartFile : fileMap.values()) {
+			String genId = UUID.randomUUID().toString();
+			String originalfileName=multipartFile.getOriginalFilename();
+			String saveFileName = genId + "." + getExtension(originalfileName);
+			String savePath = realFolder + saveFileName; // 저장 될 파일 경로
+			long fileSize = multipartFile.getSize();
+			multipartFile.transferTo(new File(savePath));
+			int res=restaurantMapper.fileUpload(originalfileName, saveFileName, fileSize,Integer.parseInt(rnum));
+			if(res>0) {
+				msg="사진 등록 성공";
+				url="restaurant_content?rnum="+rnum;
+			}else {
+				msg="사진 등록 실패";
+				url="restaurant_list";
+			}
+		}
+
+		req.setAttribute("msg", msg);
+		req.setAttribute("url", url);
+		return new ModelAndView("restaurant/message");
+	}
+	public String getExtension(String fileName) {
+		int dotPosition = fileName.lastIndexOf('.');
+
+		if (-1 != dotPosition && fileName.length() - 1 > dotPosition) {
+			return fileName.substring(dotPosition + 1);
+		} else {
+			return "";
+		}
+	}
+
+
+	@RequestMapping(value="/restaurant_list")
+	public ModelAndView listRest() throws Exception{
+		List<RestaurantDTO> list = restaurantMapper.listRest();
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("restList",list);
+		mav.setViewName("restaurant/restaurant_list");
+		return mav;
+	}
+
+	@RequestMapping(value="/restaurant_content")
+	public ModelAndView contentRest(@RequestParam int rnum){
+		RestaurantDTO dto = restaurantMapper.getRest(rnum);
+		List<uploadFileVO> uploadFileList = restaurantMapper.getFileList(rnum);
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("getRest", dto);
+		mav.addObject("uploadFileList", uploadFileList);
+		mav.setViewName("restaurant/restaurant_content");
+		return mav; 
+	}
 	
+	@RequestMapping(value="/getImage/{name:.+}",method=RequestMethod.GET)
+	@ResponseBody
+	    public ResponseEntity<byte[]> profileImage(@PathVariable("name") String name) throws IOException {
+	        HttpHeaders header = new HttpHeaders();
+	        header.setContentType(MediaType.IMAGE_JPEG);
+	        return new ResponseEntity<byte[]>(IOUtils.toByteArray(new FileInputStream(new File(uploadPath+name))), header, HttpStatus.CREATED);
+	    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- GitYeps/src/main/java/com/yeps/service/RestaurantMapper.java
+++ GitYeps/src/main/java/com/yeps/service/RestaurantMapper.java
@@ -1,12 +1,16 @@
 package com.yeps.service;
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.yeps.model.RestaurantDTO;
+import com.web.model.RestaurantDTO;
+import com.web.model.uploadFileVO;
 
 /**
  * This is not a best practices class.  It's just an example
@@ -25,6 +29,28 @@ public class RestaurantMapper {
 	public int insertRest(RestaurantDTO dto) {
 		return sqlSession.update("insertRestaurant",dto);
 	}
+	
+	public RestaurantDTO getRest(int rnum) {
+		RestaurantDTO dto=sqlSession.selectOne("getRest",rnum);
+		return dto;
+	}
+
+	public int fileUpload(String originalfileName, String saveFileName, long fileSize,int rnum) {
+	    HashMap<String, Object> hm = new HashMap<String, Object>();
+	    hm.put("originalfileName", originalfileName);
+	    hm.put("saveFileName", saveFileName);
+	    hm.put("fileSize", fileSize);
+	    hm.put("rnum", rnum);
+	    return sqlSession.update("uploadFile",hm);
+	}
+	public List<uploadFileVO> getFileList(int rnum) {
+		return sqlSession.selectList("uploadFileList",rnum);
+	}
+	
+
+
+
+
 
 }
 
