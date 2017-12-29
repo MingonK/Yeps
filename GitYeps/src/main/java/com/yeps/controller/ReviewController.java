d27a8cccf3e83c262e75a73c3f9a26aa2f7bb952
Administrator <Administrator@406-09>
2017-12-29 20:39:11 +0900

    4

--- GitYeps/src/main/java/com/yeps/controller/ReviewController.java
+++ GitYeps/src/main/java/com/yeps/controller/ReviewController.java
@@ -88,12 +88,77 @@ public class ReviewController {
 //			
 //		}
 		
-		
+
 		@RequestMapping(value="/start")
 		public String start() {
 			return "start";
 		}
 	
+		
+		@RequestMapping(value = "/review_index")
+		public ModelAndView index() {
+		
+			//최고평점을 index page로 보내주기 
+			int NBPmnum = reviewMapper.new_BestGradePoint();
+			
+			//최근활동 리뷰/사진을 index page로 보내주기
+			//★미구현:설정된 위치지역일때의 정보들을 꺼내서 index에  뿌려주기
+			//★미구현:file 부분 미구현 
+			List<ReviewDTO> recentReviewInfo = reviewMapper.recentReviewInfo();	//rvnum, gradepoint, content값 가져오는dto
+			
+//			System.out.println("recentReviewInfo의 SIze: " + recentReviewInfo.size());
+			HashMap<Integer, List<ReviewDTO>> map = new HashMap<Integer, List<ReviewDTO>>(); 
+			for(int i=0; i<recentReviewInfo.size(); i++) {
+				map.put(i, reviewMapper.numList(recentReviewInfo.get(i).getRvnum()));
+//				for(int j = 0; j < map.get(i).size(); j++) 
+//					System.out.println("맵 안에 저장된 LIST의 데이터: " + map.get(i).get(j).getMnum() + " : " + map.get(i).get(j).getRnum());
+			}
+			
+			List<MemberDTO> get_mname = new ArrayList<MemberDTO>();
+			for(int i=0; i < map.size(); i++) {
+				for(int j = 0; j < map.get(i).size(); j++) {
+					get_mname.add(memberMapper.memberName(map.get(i).get(j).getMnum()));
+				}
+			}
+			
+			List<RestaurantDTO> get_rname = new ArrayList<RestaurantDTO>();
+			for(int i=0; i < map.size(); i++) {
+				for(int j = 0; j < map.get(i).size(); j++) {
+					get_rname.add(restaurantMapper.restaurantRName(map.get(i).get(j).getRnum()));
+				}
+			}
+			
+		
+			ModelAndView mav = new ModelAndView();
+			mav.addObject("NBPmnum", NBPmnum);
+			mav.addObject("recentReviewInfo", recentReviewInfo);
+			mav.addObject("get_mnameList", get_mname);
+			mav.addObject("get_rnameList", get_rname);
+			
+			mav.setViewName("/review/index");
+			return mav;
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		@RequestMapping(value="/review_list")
