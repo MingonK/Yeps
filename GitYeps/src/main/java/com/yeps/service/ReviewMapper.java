bf964b44b874ddc9cc2eec75ef10d835f49380f0
samsung <samsung@seunGjr>
2018-01-04 16:26:09 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/MingonK/Yeps.git
    
    Conflicts:
    	GitYeps/target/m2e-wtp/web-resources/META-INF/maven/com.yeps/controller/pom.properties
    
    	리뷰메퍼xml과 restaurant_content, restaurantIMG, write.jsp 페이지와 리뷰 컨트롤러 수정
    있습니다.2017.1.4.16:25;

--- GitYeps/src/main/java/com/yeps/service/ReviewMapper.java
+++ GitYeps/src/main/java/com/yeps/service/ReviewMapper.java
@@ -13,135 +13,143 @@ import com.yeps.model.ReviewDTO;
 @Service
 public class ReviewMapper {
 
-	@Autowired
-	private SqlSession sqlSession;
+   @Autowired
+   private SqlSession sqlSession;
 
-	public List<ReviewDTO> listReview() {
-		return sqlSession.selectList("listReview");
-	}
+   public List<ReviewDTO> listReview() {
+      return sqlSession.selectList("listReview");
+   }
 
-	public int insertReview(ReviewDTO dto) {
-		return sqlSession.insert("insertReview", dto);
-	}
+   public int insertReview(ReviewDTO dto) {
+      return sqlSession.insert("insertReview", dto);
+   }
 
-	public int deleteReview(String rvnum) {
-		return sqlSession.delete("deleteReview", rvnum);
-	}
+   public int deleteReview(String rvnum) {
+      return sqlSession.delete("deleteReview", rvnum);
+   }
 
-	public int updateReview(ReviewDTO dto) {
-		return sqlSession.update("updateReview", dto);
-	}
+   public int updateReview(ReviewDTO dto) {
+      return sqlSession.update("updateReview", dto);
+   }
 
-	public ReviewDTO getReview(int rvnum) {
-		return sqlSession.selectOne("getReview", rvnum);
-	}
+   public ReviewDTO getReview(int rvnum) {
+      return sqlSession.selectOne("getReview", rvnum);
+   }
 
-	public int new_BestGradePoint() {
-		return sqlSession.selectOne("new_BestGradePoint");
-	}
+   public int new_BestGradePoint() {
+      return sqlSession.selectOne("new_BestGradePoint");
+   }
 
-	public String getSelectedRestaurant_Rname(int rnum) {
-		return sqlSession.selectOne("getgetSelectedRestaurant_Rname", rnum);
-	}
+   public String getSelectedRestaurant_Rname(int rnum) {
+      return sqlSession.selectOne("getgetSelectedRestaurant_Rname", rnum);
+   }
 
-	public List<ReviewDTO> getSelectedRestaurant_Rv(int rnum, int start, int end) {
+   public List<ReviewDTO> getSelectedRestaurant_Rv(int rnum, int start, int end) {
+      HashMap<String, Integer> map = new HashMap<String, Integer>();
+      map.put("rnum", rnum);
+      map.put("start", start);
+      map.put("end", end);
+      return sqlSession.selectList("getSelectedRestaurant_Rv", map);
+   }
+
+   public List<ReviewDTO> getRandomRestaurant_Rv(int rnum) {
+      return sqlSession.selectList("getRandomRestaurant_Rv", rnum);
+   }
+
+   public ReviewDTO review_mylist_info(int NBPmnum) {
+      return sqlSession.selectOne("review_mylist_info", NBPmnum);
+   }
+
+   public int review_mylist_updatedata(int rvnum) {
+      return sqlSession.update("review_mylist_updatedata", rvnum);
+   }
+
+   public int GetRestaurantName_Rv(int NBPmnum) {
+      return sqlSession.selectOne("GetRestaurantName_Rv", NBPmnum);
+   }
+
+   public List<ReviewDTO> recentReviewInfo() {
+      return sqlSession.selectList("recentReviewInfo");
+   }
+
+   public ReviewDTO numList(int reviewIntList) {
+      return sqlSession.selectOne("numList", reviewIntList);
+   }
+
+   public List<ReviewDTO> previous_reviews_Num() {
+      return sqlSession.selectList("previous_reviews_Num");
+   }
+
+   public List<ReviewDTO> previous_Rv() {
+      return sqlSession.selectList("previous_Rv");
+   }
+
+   public List<ReviewDTO> review_keyword(String SearchKeyword, int rnum, int start, int end) {
+      HashMap<String, Object> map = new HashMap<String, Object>();
+      map.put("SearchKeyword", SearchKeyword);
+      map.put("rnum", rnum);
+      map.put("start", start);
+      map.put("end", end);
+      return sqlSession.selectList("review_keyword", map);
+   }
+
+   public int review_keywordCount(String SearchKeyword) {
+      return sqlSession.selectOne("review_keywordCount", SearchKeyword);
+   }
+
+   public ReviewDTO getLastReview(int rnum) {
+      return sqlSession.selectOne("getLastReview", rnum);
+   }
+
+   public int getRestaurantReviewCount(int rnum) {
+      return sqlSession.selectOne("getRestaurantReviewCount", rnum);
+   }
+
+   public int getStarAvg(int rnum) {
+      if (sqlSession.selectOne("getStarAvg", rnum) == null) {
+         return 0;
+      }
+      return sqlSession.selectOne("getStarAvg", rnum);
+   }
+
+   public List<Integer> getSelectedRestaurant_Rv_M(int rnum) {
+      return sqlSession.selectList("getSelectedRestaurant_Rv_M", rnum);
+   }
+
+   public List<ReviewDTO> listRestReview(int start, int end) {
+      Map<String, Object> map = new HashMap<String, Object>();
+      map.put("start", start);
+      map.put("end", end);
+      return sqlSession.selectList("listRestaurantReview", map);
+   }
+
+   public String Get_InsertReviewDate() {
+      return sqlSession.selectOne("Get_InsertReviewDate");
+   }
+
+   // 12월 31일 지원 추가
+   public List<Integer> gethotAndNewReview() {
+      return sqlSession.selectList("gethotAndNewReview");
+   }
+
+   // 1월 3일 상우 추가
+   public int getPreviousReviewCount() {
+      return sqlSession.selectOne("getPreviousReviewCount");
+   }
+
+   public List<ReviewDTO> previous_Rv(int start, int end) {
+      HashMap<String, Integer> map = new HashMap<String, Integer>();
+      map.put("start", start);
+      map.put("end", end);
+      return sqlSession.selectList("previous_Rv", map);
+   }
+
+   // 2017.1.4 승지 추가
+   public List<ReviewDTO> getMemberReview(int mnum, int start, int end){
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		map.put("rnum", rnum);
+		map.put("mnum", mnum);
 		map.put("start", start);
 		map.put("end", end);
-		return sqlSession.selectList("getSelectedRestaurant_Rv", map);
+		return sqlSession.selectList("getMemberReview",map);
 	}
-
-	public List<ReviewDTO> getRandomRestaurant_Rv(int rnum) {
-		return sqlSession.selectList("getRandomRestaurant_Rv", rnum);
-	}
-
-	public ReviewDTO review_mylist_info(int NBPmnum) {
-		return sqlSession.selectOne("review_mylist_info", NBPmnum);
-	}
-
-	public int review_mylist_updatedata(int rvnum) {
-		return sqlSession.update("review_mylist_updatedata", rvnum);
-	}
-
-	public int GetRestaurantName_Rv(int NBPmnum) {
-		return sqlSession.selectOne("GetRestaurantName_Rv", NBPmnum);
-	}
-
-	public List<ReviewDTO> recentReviewInfo() {
-		return sqlSession.selectList("recentReviewInfo");
-	}
-
-	public ReviewDTO numList(int reviewIntList) {
-		return sqlSession.selectOne("numList", reviewIntList);
-	}
-
-	public List<ReviewDTO> previous_reviews_Num() {
-		return sqlSession.selectList("previous_reviews_Num");
-	}
-
-	public List<ReviewDTO> previous_Rv() {
-		return sqlSession.selectList("previous_Rv");
-	}
-
-	public List<ReviewDTO> review_keyword(String SearchKeyword, int rnum, int start, int end) {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("SearchKeyword", SearchKeyword);
-		map.put("rnum", rnum);
-		map.put("start", start);
-		map.put("end", end);
-		return sqlSession.selectList("review_keyword", map);
-	}
-
-	public int review_keywordCount(String SearchKeyword) {
-		return sqlSession.selectOne("review_keywordCount", SearchKeyword);
-	}
-
-	public ReviewDTO getLastReview(int rnum) {
-		return sqlSession.selectOne("getLastReview", rnum);
-	}
-
-	public int getRestaurantReviewCount(int rnum) {
-		return sqlSession.selectOne("getRestaurantReviewCount", rnum);
-	}
-
-	public int getStarAvg(int rnum) {
-		if (sqlSession.selectOne("getStarAvg", rnum) == null) {
-			return 0;
-		}
-		return sqlSession.selectOne("getStarAvg", rnum);
-	}
-
-	public List<Integer> getSelectedRestaurant_Rv_M(int rnum) {
-		return sqlSession.selectList("getSelectedRestaurant_Rv_M", rnum);
-	}
-
-	public List<ReviewDTO> listRestReview(int start, int end) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("start", start);
-		map.put("end", end);
-		return sqlSession.selectList("listRestaurantReview", map);
-	}
-
-	public String Get_InsertReviewDate() {
-		return sqlSession.selectOne("Get_InsertReviewDate");
-	}
-
-	// 12월 31일 지원 추가
-	public List<Integer> gethotAndNewReview() {
-		return sqlSession.selectList("gethotAndNewReview");
-	}
-
-	// 1월 3일 상우 추가
-	public int getPreviousReviewCount() {
-		return sqlSession.selectOne("getPreviousReviewCount");
-	}
-
-	public List<ReviewDTO> previous_Rv(int start, int end) {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		map.put("start", start);
-		map.put("end", end);
-		return sqlSession.selectList("previous_Rv", map);
-	}
-
 }
