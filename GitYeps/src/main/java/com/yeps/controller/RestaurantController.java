08f07eeece3ac7cbfc2db61eebad5cb757524fa2
Mingon <Mingon@MinGon-PC>
2018-01-05 02:21:31 +0900

    리뷰부분 내가 쓴 리뷰 찾는거 추가요

--- GitYeps/src/main/java/com/yeps/controller/RestaurantController.java
+++ GitYeps/src/main/java/com/yeps/controller/RestaurantController.java
@@ -196,11 +196,21 @@ public class RestaurantController {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("count", count); // 레코드의 갯수
 		map.put("YepsPager", YepsPager);
+		
+		HttpSession session = req.getSession();
+		MemberDTO loginMember = (MemberDTO) session.getAttribute("memberinfo");
 
 		RestaurantDTO getRest = restaurantMapper.getRest(Integer.parseInt(rnum));// 가게 1개 정보
 		List<FileDTO> uploadFileList = restaurantMapper.getFileList(Integer.parseInt(rnum));// 가게 업로드 파일
-
+		
+		boolean existMyReview = reviewMapper.findMyReview(Integer.parseInt(rnum), loginMember.getMnum());
 		List<ReviewDTO> reviewList = reviewMapper.getSelectedRestaurant_Rv(Integer.parseInt(rnum), start, end);// 가게 리뷰
+		if(existMyReview) {
+			// 내가 쓴 리뷰가 존재하니까 그거 하나만 따로 저장해 놓기
+		} else {
+			// 없으면 그냥 진행
+		}
+		
 		List<ReviewDTO> highlightReview = reviewMapper.getRandomRestaurant_Rv(Integer.parseInt(rnum));
 		int starAvg = reviewMapper.getStarAvg(Integer.parseInt(rnum));
 
